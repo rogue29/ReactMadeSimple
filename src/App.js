eba199cf049a471267f0cb72af215c630d8a6466
rogue29 <rohitggulyani@gmail.com>
2020-03-25 18:56:37 +0530

    Further dynamic : Person gets property from state.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons : [
+      {name:"Rohit", age:30},
+      {name:"Bharti", age:25},
+      {name:"Roharti", age:55}
+    ],
+  };
+
   render() {
     return (
       <div className="App">
         <h1>This is a test person page.</h1>
-        <Person name="Rohit" age="30" />
-        <Person name="Bharti" age="25" />
-        <Person name="Roharti" age="55">Janmo Janmo ka nata</Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Janmo Janmo ka nata</Person>
       </div>
     );
   }
